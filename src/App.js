4fde27cb514de8a4341b4af0f1129e796f8491c9
Naresh <naresh@anywhere.co>
2023-06-12 15:58:33 +0530

    removed conflicts

--- src/App.js
+++ src/App.js
@@ -1,38 +1,11 @@
-<<<<<<< HEAD
 import React, { Suspense, lazy } from "react";
 import "./App.css";
-// import StaticData from "./dynamicModule";
-const LazyLoadedComponent = lazy(() => import("./LazyLoadedComponent"));
-=======
-import logo from "./logo.svg";
-import "./App.css";
->>>>>>> f12b1518f53371e17b6f58667069b09ae4c162c2
-
+import Main from "./ts/main.tsx";
 function App() {
   return (
-    <div className="App">
-<<<<<<< HEAD
-      <header className="App-header">Vanity Numbers1</header>
-
-      <Suspense fallback={<div>loading</div>}>
-        <LazyLoadedComponent />
-      </Suspense>
-=======
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React!
-        </a>
-      </header>
->>>>>>> f12b1518f53371e17b6f58667069b09ae4c162c2
+    <div>
+      from App
+      <Main />
     </div>
   );
 }
